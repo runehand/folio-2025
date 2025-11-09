5ff82e6f8346abc962def2c048d743b83ca5415a
brunosimon <simon.bruno.77@gmail.com>
2025-11-09 12:09:56 +0100

    :bento: Audio > Add fan pop

--- sources/Game/World/Areas/SocialArea.js
+++ sources/Game/World/Areas/SocialArea.js
@@ -152,6 +152,9 @@ export class SocialArea extends Area
 
             this.fans.visibleCount = Math.min(this.fans.visibleCount + 1, this.fans.count)
 
+            // Sound
+            this.game.audio.groups.get('click')[0].play(true)
+
             // Achievement
             this.game.achievements.setProgress('fan', 1)
         }
